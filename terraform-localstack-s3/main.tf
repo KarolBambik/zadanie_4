9d8dfe173f0ce2e193d992783a97d2bb2048557c
Qdellsky <127014062+KarolBambik@users.noreply.github.com>
2024-05-26 21:11:20 +0200

    Add files via upload

---
+++ terraform-localstack-s3/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "us-east-1"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-bucket"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
